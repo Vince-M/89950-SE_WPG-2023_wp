dfaa5d6cc55c49c062530b508d2176d3aacd3c55
Vince Mendella <vmendella@gmail.com>
2024-05-15 11:26:17 -0500

    add header.php and footer.php files

---
+++ footer.php
@@ -0,0 +1,66 @@
+<!-- FOOTER
+  =================================================== -->
+  <footer class="footer">
+    <div class="wrapper row">
+      <div class="footer__nav">
+        <div class="footer__logo">
+          <a href="index.html">
+            <img src="img/head-logo.svg" alt="WPG_logo">
+          </a>
+        </div>
+
+        <div class="footer__menu">
+          <div class="footer__nav--item footer__nav--conference">
+            <h4 class="footer__menu--head">Programs</h4>
+            <ul title="Conference">
+              <li><a href="schedule.html">Conference Schedule</a></li>
+              <li><a href="victoria.html">Pharmacy Schedule</a></li>
+              <li><a href="city.html">WPG Concert Series</a></li>
+            </ul>
+          </div>
+
+          <div class="footer__nav--item footer__nav--tradeshow">
+            <h4 class="footer__menu--head">Plan your Visit</h4>
+            <ul title="Tradeshow">
+              <li><a href="victoria.html#">Victoria Inn</a></li>
+              <li><a href="vendors.html">In the City</a></li>
+            </ul>
+          </div>
+
+          <div class="footer__nav--item footer__nav--featureItems">
+            <h4 class="footer__menu--head">Trade Show</h4>
+            <ul title="Feature Items">
+              <li><a href="sponsors.html">Sponsors</a></li>
+              <li><a href="#">Vendors</a></li>
+              <li><a href="#">Trade Show Rotations</a></li>
+            </ul>
+          </div>
+
+            <!-- contact button -->
+            <div class="btn btn__contact btn__contact--footer nav__link">
+              <a class="btn btn__contact nav__link" href="contact.html">Contact</a>
+            </div>
+            <!-- contact button end -->
+        </div>
+        <!-- footer__menu -->
+      </div>
+      <!-- footer__nav -->
+      <div class="footer__media">
+        <div class="footer__media--copy">
+          <p>Copyright &copy; 2024 NWC Wintering Partners Gathering. All rights reserved.</p>
+        </div>
+        <div class="footer__media--links">
+          <a href="#">Legal / Privacy</a>
+          <a href="#">Terms & Conditions</a>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+    </div>
+    <!-- container row -->
+  </footer>
+  <!-- ============================================== -->
+
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NWC Conference & Trade Show</title>
+
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  
+  <link rel="stylesheet" href="dist/style.css">
+  <script defer src="dist/script.js"></script>
+
+  <?php wp_head(); ?>
+
+
+</head>
+<body <?php body_class(); ?>>
+      <?php wp_body_open(); ?>
+  <a href="#main" class="visually-hidden">Skip to main content</a>
+
+  <!-- HEADER / NAVIGATION SECTION START
+  =================================================== -->
+  <header class="header masthead">
+    <div class="overlay has-fade"></div>
+    <!-- overlay has-fade -->
+
+    <div class="wrapper header__section">
+      <nav class="nav" role="navigation" aria-label="Main">
+        
+        <div class="header__logo site-branding">
+          <a href="index.html">
+            <img src="img/head-logo.svg" alt="WPG logo 2024" width="140">
+          </a>
+        </div>
+        <!-- header__logo -->
+
+        <a id="btnHamburger" class="hamburger header__toggle hide-for-desktop" href="#">
+          <span></span>
+          <span></span>
+          <span></span>
+        </a> <!-- nav__toggle header__menu hide-for-desktop -->
+
+        <!-- header menu -->
+        <div class="header__nav hide-for-mobile">
+          <div class="header__menu">
+            <ul class="menu nav__list">
+              <li class="nav__item has-submenu">
+                <a tabindex="0">Programs &#8964;</a>
+                <ul class="submenu">
+                  <li class="subitem"><a href="#">Conference Schedule</a></li>
+                  <li class="subitem"><a href="victoria.html">Pharmacy Schedule</a></li>
+                  <li class="subitem"><a href="concert.html">WPG Concert Series</a></li>
+                </ul>
+              </li>
+
+              <li class="nav__item has-submenu">
+                <a tabindex="0">Plan your Visit &#8964;</a>
+                <ul class="submenu">
+                  <li class="subitem"><a href="victoria.html">Victoria Inn</a></li>
+                  <li class="subitem"><a href="city.html">In the City</a></li>
+                </ul>
+              </li>
+
+              <li class="nav__item has-submenu">
+                <a tabindex="0">Trade Show &#8964;</a>
+                <ul class="submenu">
+                  <li class="subitem"><a href="sponsors.html">Sponsors</a></li>
+                  <li class="subitem"><a href="#">Vendors</a></li>
+                  <li class="subitem"><a href="#">Trade Show Rotations</a></li>
+                </ul>
+              </li>
+
+            </ul>
+            <!-- contact button -->
+            <div class="btn btn__contact nav__link">
+              <a class="btn btn__contact nav__link" href="contact.html">Contact</a>
+            </div>
+            <!-- contact button end -->
+          </div>
+        </div>
+        <!-- header__nav -->
+      </nav>
+      <!-- nav -->
+    </div>
+    <!-- container header__section -->
+  </header> <!-- HEADER / NAVIGATION SECTION END -->
+  <!-- ============================================== -->
