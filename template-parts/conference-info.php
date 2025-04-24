00239c35a7cdfa778693bac59b41650f2b401a02
Vince Mendella <vmendella@gmail.com>
2025-04-24 15:04:51 -0500

    add template-part for hero conference information

---
+++ template-parts/conference-info.php
@@ -0,0 +1,16 @@
+<div class="wrapper hero__index">
+  <div class="hero__indexContent">
+    <div class="hero__content">
+      <div class="hero__branding">
+        <img src="<?php echo get_theme_file_uri() ?>/img/hero_branding_2025.svg" alt="WPG logo" class="hero-brand">
+      </div> <!-- hero__branding -->
+      <div class="hero__info">
+        <p class="hero__date">May 26 - 30, 2025</p>
+        <p class="hero__location">
+          <span class="hero__hotel">Victoria Inn</span><br>
+          <span class="hero__place">Hotel & Conference Centre<br>
+          Winnipeg, Manitoba</span></p>
+      </div> <!-- hero__info -->
+    </div> <!-- hero__content -->
+  </div> <!-- hero__index -->
+</div> <!-- wrapper -->
